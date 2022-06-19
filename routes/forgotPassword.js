c0e4ab4a931905143319035e62b5890bf419250c
Taranpreet <taranpreetsingh29@gmail.com>
2022-06-19 17:16:17 +0530

    Minor code cleanup

--- routes/forgotPassword.js
+++ routes/forgotPassword.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
-import fetch from 'node-fetch'
 import { User } from '../models/user.js'
 
 const router = express.Router()
